26ed7cc11115637a6fe7ecbb0df9ae4ff751a62b
joseduarte649 <junior12025p@gmail.com>
2025-03-10 21:46:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+
+    envelope.click(function() {
+        openEnvelope();
+    });
+
+    btn_open.click(function() {
+        openEnvelope();
+    });
+
+    btn_reset.click(function() {
+        closeEnvelope();
+    });
+
+    function openEnvelope() {
+        envelope.addClass("open").removeClass("close");
+    }
+
+    function closeEnvelope() {
+        envelope.addClass("close").removeClass("open");
+    }
+});
